b3724c4a2048fb7412c8bf7f4d9a81ca138debb3
Ryotaro Onuki <kerikun11+github@gmail.com>
2018-11-06 23:54:15 +0900

    ダイソーのクライアントを作成

--- main/main.cpp
+++ main/main.cpp
@@ -1,56 +1,74 @@
-#include <BLE2902.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <FreeRTOS.h>
 #include <iostream>
-#include <sstream>
-#include <string>
 
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#include <PowerManagement.h>
+
+#include <BLE2902.h>
+#include <BLE2904.h>
+#include <BLEClient.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
 
 #include <esp_log.h>
 constexpr static const char *tag = "app_main";
 
+#include <FreeRTOS.h>
+#include <GPIO.h>
+using namespace ESP32CPP;
+
+static BLEUUID GATT_HID((uint16_t)0x1812);
+static BLEUUID GATT_HID_REPORT((uint16_t)0x2a4d);
+
+static BLEAddress *pServerAddress = NULL;
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    ESP_LOGI(tag, "onResult: %s", advertisedDevice.getName().c_str());
+    if (advertisedDevice.getName().find("AB Shutter3") == 0) {
+      advertisedDevice.getScan()->stop();
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress());
+      ESP_LOGI(tag, "found device: %s", pServerAddress->toString().c_str());
+    }
+  }
+};
+
+static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
+                           uint8_t *pData, size_t length, bool isNotify) {
+  uint16_t value = pData[0] << 8 | pData[1];
+  ESP_LOGI(tag, "0x%04X", value);
+}
+
 extern "C" void app_main() {
+  PowerManagement::init();
+  gpio_num_t pin = GPIO_NUM_27;
+  GPIO::setOutput(pin);
+  GPIO::write(pin, 1);
+
   BLEDevice::init("MyESP32");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ |
-                               BLECharacteristic::PROPERTY_WRITE |
-                               BLECharacteristic::PROPERTY_NOTIFY);
-  pCharacteristic->addDescriptor(new BLE2902());
-  BLEDescriptor *UserDescription = new BLEDescriptor(BLEUUID((uint16_t)0x2901));
-  UserDescription->setValue("Hoge");
-  pCharacteristic->addDescriptor(UserDescription);
+  BLEScan *pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(30);
 
-  pCharacteristic->setValue("Hello World says Neil");
-  class MyBLECharacteristicCallback : public BLECharacteristicCallbacks {
-    void onRead(BLECharacteristic *pCharacteristic) override {
-      ESP_LOGI(tag, "onRead!");
-    }
-    void onWrite(BLECharacteristic *pCharacteristic) override {
-      ESP_LOGI(tag, "onWrite");
-    }
-  };
-  pCharacteristic->setCallbacks(new MyBLECharacteristicCallback());
-  pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
-  ESP_LOGI(tag, "Characteristic defined! Now you can read it in your phone!");
-
-  int cnt = 0;
   while (1) {
-    auto str = pCharacteristic->getValue();
-    std::cout << str << std::endl;
-    std::stringstream ss;
-    ss << "Counter: " << cnt;
-    pCharacteristic->setValue(ss.str());
-    pCharacteristic->notify();
+    FreeRTOS::sleep(10);
+    static bool connected = false;
 
-    cnt++;
-    FreeRTOS::sleep(1000);
+    if (pServerAddress != NULL && !connected) {
+      BLEClient *pClient = BLEDevice::createClient();
+      pClient->connect(*pServerAddress);
+
+      BLERemoteService *pRemoteService = pClient->getService(GATT_HID);
+      if (pRemoteService) {
+        auto *pCharacteristicMap = pRemoteService->getCharacteristics();
+        for (auto itr : *pCharacteristicMap) {
+          ESP_LOGI(tag, "%s", itr.second->toString().c_str());
+          if (GATT_HID_REPORT.equals(itr.second->getUUID()) &&
+              itr.second->canNotify()) {
+            itr.second->registerForNotify(notifyCallback);
+            ESP_LOGI(tag, "registered");
+          }
+        }
+        connected = true;
+      }
+    }
   }
 }
